96e797031980b8efa1fcb69f0cece6488f0600a6
LucasEPaduam <79526125+LucasEPaduam@users.noreply.github.com>
2022-09-19 15:41:43 -0300

    Adicionar arquivos de projeto.

---
+++ ConjuntoDeInteiros.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LP2_EX3_ConjuntoDeInteiros
+{
+    class ConjuntoDeInteiros
+    {
+
+        private List<bool> conjunto;
+        private string conjuntoImpresso;
+
+        public ConjuntoDeInteiros()
+        {
+            this.conjunto = new List<bool>();
+        }
+
+        public List<bool> uniao(int tamanhoDoConjunto, List<int> conjunto1, List<int> conjunto2)
+        {
+            this.conjunto = tamanhoDaLista(tamanhoDoConjunto);
+
+
+            for (int i = 0; i < this.conjunto.Count; i++)
+            {
+
+                if (conjunto1.Contains(i) || conjunto2.Contains(i))
+                {
+                    this.conjunto[i] = true;
+                }
+
+            }
+
+            return this.conjunto;
+        }
+
+        public List<bool> intersecao(int tamanhoDoConjunto, List<int> conjunto1, List<int> conjunto2)
+        {
+            this.conjunto = tamanhoDaLista(tamanhoDoConjunto);
+
+
+            for (int i = 0; i < this.conjunto.Count; i++)
+            {
+
+                if (conjunto1.Contains(i) && conjunto2.Contains(i))
+                {
+                    this.conjunto[i] = true;
+                }
+
+            }
+
+            return this.conjunto;
+        }
+
+        public List<bool> insereElemento(int tamanhoDoConjunto, int elemento, List<int> conjunto)
+        {
+
+            this.conjunto = tamanhoDaLista(tamanhoDoConjunto);
+            for (int i = 0; i < this.conjunto.Count; i++)
+            {
+
+                if (conjunto.Contains(i))
+                {
+                    this.conjunto[i] = true;
+                }
+
+            }
+
+            conjunto.Add(elemento);
+            this.conjunto[elemento] = true;
+
+            return this.conjunto;
+
+        }
+
+        public List<bool> deletaElemento(int tamanhoDoConjunto, int elemento, List<int> conjunto)
+        {
+
+            this.conjunto = tamanhoDaLista(tamanhoDoConjunto);
+            for (int i = 0; i < this.conjunto.Count; i++)
+            {
+
+                if (conjunto.Contains(i))
+                {
+                    this.conjunto[i] = true;
+                }
+
+            }
+
+            conjunto.Remove(elemento);
+            this.conjunto[elemento] = false;
+
+            return this.conjunto;
+
+        }
+
+        public string ToSetString(List<int> conjuntoExpresso)
+        {
+            if (conjuntoExpresso.Count != 0)
+            {
+                foreach (int item in conjuntoExpresso)
+                {
+                    conjuntoImpresso += item + "-";
+                }
+
+                conjuntoImpresso = "CONJUNTO {" + conjuntoImpresso + "}";
+
+                return conjuntoImpresso;
+            }
+            else
+            {
+                return "CONJUNTO { - }";
+            }
+        }
+
+        public Boolean conjuntoIsEqual(List<int> conjunto1, List<int> conjunto2)
+        {
+
+            if (conjunto1.Count != conjunto2.Count)
+            {
+                for (int i = 0; i < conjunto1.Count; i++)
+                {
+                    if (conjunto1[i] != conjunto2[i])
+                    {
+                        return false;
+                    }
+
+                    
+                }
+
+                return false;
+            }
+            return true;
+        }
+
+
+        private List<bool> tamanhoDaLista(int tamanho)
+        {
+            this.conjunto.Clear();
+            for (int i = 0; i <= tamanho; i++)
+            {
+                this.conjunto.Add(false);
+            }
+
+            return this.conjunto;
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LP2_EX3_ConjuntoDeInteiros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConjuntoDeInteiros conjunto = new ConjuntoDeInteiros();
+            List<int> conjuntoInteiros1 = new List<int>();
+            List<int> conjuntoInteiros2 = new List<int>();
+
+            conjuntoInteiros1.Add(1);
+            conjuntoInteiros1.Add(5);
+            conjuntoInteiros1.Add(7);
+            conjuntoInteiros1.Add(8);
+
+            conjuntoInteiros2.Add(1);
+            conjuntoInteiros2.Add(5);
+            conjuntoInteiros2.Add(7);
+            conjuntoInteiros2.Add(8);
+
+            Console.WriteLine(conjunto.ToSetString(conjuntoInteiros1));
+            Console.WriteLine(conjunto.ToSetString(conjuntoInteiros2));
+
+            Console.WriteLine("UNIAO");
+
+            foreach (bool item in conjunto.uniao(10, conjuntoInteiros1, conjuntoInteiros2))
+            {
+                Console.Write(item);
+            }
+            Console.WriteLine("\nINTERSECCAO");
+            foreach (bool item2 in conjunto.intersecao(10, conjuntoInteiros1, conjuntoInteiros2))
+            {
+                Console.Write(item2);
+            }
+
+            Console.WriteLine("\nINSERE ELEMENTO");
+            foreach (bool item3 in conjunto.insereElemento(10, 10, conjuntoInteiros1))
+            {
+                Console.Write(item3);
+            }
+
+            Console.WriteLine("\nDELETA ELEMENTO");
+            foreach (bool item4 in conjunto.deletaElemento(10, 7, conjuntoInteiros1))
+            {
+                Console.Write(item4);
+            }
+
+            Console.WriteLine("\nOS CONJUNTOS SÃO IGUAIS? " + conjunto.conjuntoIsEqual(conjuntoInteiros1, conjuntoInteiros2));
+        }
+    }
+}
